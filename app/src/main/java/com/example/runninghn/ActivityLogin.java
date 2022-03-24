2cb34ab93c44777d23f178860da48a76c6cc6c55
Jennifer Varela <50208658+JenniferVarela@users.noreply.github.com>
2022-03-23 21:56:15 -0600

    Actividad login

---
+++ app/src/main/java/com/example/runninghn/ActivityLogin.java
@@ -0,0 +1,14 @@
+package com.example.runninghn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
