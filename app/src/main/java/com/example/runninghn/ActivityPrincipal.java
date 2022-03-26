41e7b66e4ee51f92227661ddea977dcd47411205
Jennifer Varela <50208658+JenniferVarela@users.noreply.github.com>
2022-03-26 00:03:59 -0600

    se modifico pantalla de perfil de corredor, y se creo la actividad para el xml de la pantalla principal, prueba de menu en pantalla registrar

---
+++ app/src/main/java/com/example/runninghn/ActivityPrincipal.java
@@ -0,0 +1,14 @@
+package com.example.runninghn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
