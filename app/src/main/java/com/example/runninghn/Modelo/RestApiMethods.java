6ee4f0637e889182ada30e82309230c5f02afd97
estebancerrato <estebancerrato@hotmail.com>
2022-03-27 15:24:45 -0600

    se agrego Activity_Perfil

--- app/src/main/java/com/example/runninghn/Modelo/RestApiMethods.java
+++ app/src/main/java/com/example/runninghn/Modelo/RestApiMethods.java
@@ -6,6 +6,8 @@ public class RestApiMethods {
     //EndPoint Urls
     private static final String GetEmple = "/APIexam/listacontactos.php";
     private static final String GetBuscar = "/API/validarLogin.php";
+    private static final String getBuscarCorreo = "/API/listasingleusuario.php";
+
     private static final String setUpdate = "/APIexam/actualizarcontacto.php";
     private static final String CreateUsuario = "/API/crearusuario.php";
     private static final String listaPaises = "/API/listapaises.php";
@@ -13,6 +15,8 @@ public class RestApiMethods {
     //metodo get
     public static final String EndPointGetContact = StringHttp + ipaddress + GetEmple;
     public static final String EndPointValidarLogin = StringHttp + ipaddress + GetBuscar;
+    public static final String EndPointBuscarCorreo = StringHttp + ipaddress + getBuscarCorreo;
+
     public static final String EndPointSetUpdateContact = StringHttp + ipaddress + setUpdate;
     public static final String EndPointCreateUsuario = StringHttp + ipaddress + CreateUsuario;
     public static final String EndPointListarPaises = StringHttp + ipaddress + listaPaises;
--- app/src/main/java/com/example/runninghn/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/runninghn/ui/home/HomeFragment.java
@@ -1,21 +1,44 @@
 package com.example.runninghn.ui.home;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.runninghn.MainActivity;
+import com.example.runninghn.Modelo.RestApiMethods;
+import com.example.runninghn.Modelo.Usuario;
 import com.example.runninghn.R;
 import com.example.runninghn.databinding.ActivityMain2Binding;
 import com.example.runninghn.databinding.FragmentHomeBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class HomeFragment extends Fragment {
     ImageView foto;
@@ -39,6 +62,7 @@ public class HomeFragment extends Fragment {
         altura = (EditText) binding.fhaltura;
         fechaNac = (EditText) binding.fhfecha;
         foto = (ImageView) binding.fhImage;
+        nombre.setText("prueba");
 
         //final TextView textView = binding.textHome;
         //homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
@@ -50,6 +74,7 @@ public class HomeFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
 
 
 }
