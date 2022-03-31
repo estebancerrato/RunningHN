7e4a4e0532d92e8db5023d4d29b1f130a004969a
caste23 <castellanos.xperta@gmail.com>
2022-03-30 23:48:27 -0600

    Actualizacion

--- app/src/main/java/com/example/runninghn/Activity_Actualizar_Perfil.java
+++ app/src/main/java/com/example/runninghn/Activity_Actualizar_Perfil.java
@@ -6,21 +6,27 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -39,6 +45,7 @@ import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -46,7 +53,19 @@ import java.util.regex.Pattern;
 
 public class Activity_Actualizar_Perfil extends AppCompatActivity {
 
-    EditText txtNombre,txtApellido,txtFechaNac,txtPeso,txtAltura;
+    //Datos para fecha
+
+    private static final String TAG = "MainActivity";
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+
+    //
+
+
+
+
+    EditText txtNombre,txtApellido,txtPeso,txtAltura;
+    TextView txtFechaNac;
     Spinner SpiPais;
     Button btnActualizar,btnTomarFoto,btnSelectGaleria,btnAtras;
     ImageView Foto;
@@ -66,9 +85,51 @@ public class Activity_Actualizar_Perfil extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actualizar_perfil);
 
+
+
+        //Inicio de codigo Fecha-------------------------------------------------------------
+        mDisplayDate = (TextView) findViewById(R.id.actuFechaNac);
+
+        //--------------------------------------------------------
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        Activity_Actualizar_Perfil.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: mm/dd/yyy: " + day  + "/" + month + "/" + year);
+
+                String date =  day  + "/" + month + "/" + year;
+                mDisplayDate.setText(date);
+            }
+        };
+
+
+
+
+        //---------------------Fin de codigo de la fecha-------------------------------------------
+        //-----------------------------------------------------------------------------------------
+
+
         txtNombre = (EditText) findViewById(R.id.actutxtnombre);
         txtApellido =  (EditText) findViewById(R.id.actutxtapellidos);
-        txtFechaNac =  (EditText) findViewById(R.id.actuFechaNac);
+        txtFechaNac =  (TextView) findViewById(R.id.actuFechaNac);
         SpiPais =  (Spinner)  findViewById(R.id.actucmbPais);
         txtPeso =  (EditText) findViewById(R.id.actuPeso);
         txtAltura =  (EditText) findViewById(R.id.actuAltura);
@@ -143,6 +204,10 @@ public class Activity_Actualizar_Perfil extends AppCompatActivity {
         });
 
     }
+
+
+
+
 
     public void mostrarFoto(String foto) {
         try {
@@ -321,5 +386,6 @@ public class Activity_Actualizar_Perfil extends AppCompatActivity {
         });
         queue.add(stringRequest);
     }
+
 
 }
