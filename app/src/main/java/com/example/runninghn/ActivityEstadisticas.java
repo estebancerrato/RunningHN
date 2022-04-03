8a69615696b4565ad9d54813fae4da3619a4e191
Sergio Castillo <sergioagustincastillo@gmail.com>
2022-04-02 23:44:01 -0600

    Creada pantalla estadisticas.

---
+++ app/src/main/java/com/example/runninghn/ActivityEstadisticas.java
@@ -0,0 +1,14 @@
+package com.example.runninghn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityEstadisticas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estadisticas);
+    }
+}
