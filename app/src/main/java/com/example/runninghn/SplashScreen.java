f61de8d0cd022532b80652be4660f2af3ecdc1c1
Jennifer Varela <50208658+JenniferVarela@users.noreply.github.com>
2022-04-03 20:09:38 -0600

    Permisos de gps desde que entra a la aplicacion.

--- app/src/main/java/com/example/runninghn/SplashScreen.java
+++ app/src/main/java/com/example/runninghn/SplashScreen.java
@@ -1,27 +1,48 @@
 package com.example.runninghn;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.runninghn.Modelo.RestApiMethods;
 
 public class SplashScreen extends AppCompatActivity {
     SharedPreferences mSharedPrefs;
+    static final int LOCATION_GPS_REQUEST = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+//==========VALIDAR PERMISOS DE GPS ==============//
 
+       if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
+           ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+        //========== entrar a los anuncios despues de aceptar los permisos de gps==============
+           entrarAnuncios();
+
+        }else{
+
+          entrarAnuncios();
+       }
+}
+
+    private void entrarAnuncios() {
         mSharedPrefs = getSharedPreferences("anuncios", Context.MODE_PRIVATE);
         String olvidar = mSharedPrefs.getString("olvidar","");
-
 
         new Handler().postDelayed(new Runnable () {
             @Override
